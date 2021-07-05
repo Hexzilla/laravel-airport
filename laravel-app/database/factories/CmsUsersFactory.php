c7a4d8527b68d571b30ba0d53bb9f9852b97c319
Andrés Bestard <a.bestard@innovation.es>
2021-07-05 13:31:59 +0200

    cmsuser definition

--- laravel-app/database/factories/CmsUsersFactory.php
+++ laravel-app/database/factories/CmsUsersFactory.php
@@ -22,16 +22,18 @@ class CmsUsersFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->word,
-        'photo' => $this->faker->word,
-        'email' => $this->faker->word,
-        'password' => $this->faker->word,
-        'id_cms_privileges' => $this->faker->randomDigitNotNull,
-        'created_at' => $this->faker->date('Y-m-d H:i:s'),
-        'updated_at' => $this->faker->date('Y-m-d H:i:s'),
-        'status' => $this->faker->word,
-        'job_title' => $this->faker->word,
-        'objectguid' => $this->faker->word
+            'name' => $this->faker->name,
+            'photo' => $this->faker->word,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'id_cms_privileges' => $this->faker->randomDigitNotNull,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'status' => $this->faker->word,
+            'job_title' => $this->faker->word,
+            'objectguid' => $this->faker->word,
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ];
     }
 }
