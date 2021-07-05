eb46b7d3a2875a28d0174aef7dd77a3ebcf4b0b3
Joseph A. Poehlein López <ja.poehlein@innovation.es>
2021-07-05 12:19:44 +0200

    WIP - Added admin controller.

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +21,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [
-    HomeController::class, 'index'
-])->name('home');
+Route::get('/admin', [
+    AdminController::class, 'index'
+])->name('admin');
 
 Route::resource('cmsApiCustoms', App\Http\Controllers\CmsApiCustomController::class);
 
