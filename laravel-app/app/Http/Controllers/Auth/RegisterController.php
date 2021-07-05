e0f24f1e50ba14183f1183df3ef17d6a58a22162
Joseph A. Poehlein López <ja.poehlein@innovation.es>
2021-07-05 16:24:00 +0200

    Fixed redirects.

--- laravel-app/app/Http/Controllers/Auth/RegisterController.php
+++ laravel-app/app/Http/Controllers/Auth/RegisterController.php
@@ -60,7 +60,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\Models\CmsUsers
      */
     protected function create(array $data)
     {
