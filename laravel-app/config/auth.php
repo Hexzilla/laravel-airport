70b3656918380a1da6b79a1847d6d99794a6df51
Andrés Bestard <a.bestard@innovation.es>
2021-07-05 15:00:22 +0200

    cms_users added to auth

--- laravel-app/config/auth.php
+++ laravel-app/config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'cms_users',
     ],
 
     /*
@@ -38,12 +38,12 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'cms_users',
         ],
 
         'api' => [
             'driver' => 'token',
-            'provider' => 'users',
+            'provider' => 'cms_users',
             'hash' => false,
         ],
     ],
@@ -94,7 +94,7 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
+            'provider' => 'cms_users',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,
