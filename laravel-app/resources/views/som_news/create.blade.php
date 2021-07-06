848b87bcdb0ee2e828eb308a3207695f5048c9d9
Joseph A. Poehlein López <ja.poehlein@innovation.es>
2021-07-06 12:32:31 +0200

    Added styling, buttons, and icons to news pages.

--- laravel-app/resources/views/som_news/create.blade.php
+++ laravel-app/resources/views/som_news/create.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>Create Som News</h1>
+                    <h1><i class="far fa-newspaper ml-2"></i> Add News</h1>
                 </div>
             </div>
         </div>
@@ -13,11 +13,23 @@
 
     <div class="content px-3">
 
+        <div class="row">
+            <div class="col-12-lg ml-2 mb-4">
+                <a href="{{ route('somNews.index') }}">
+                    <i class="fa fa-chevron-left"></i> Back To List Data News
+                </a>
+            </div>
+        </div>
+
         @include('adminlte-templates::common.errors')
 
         <div class="card">
 
             {!! Form::open(['route' => 'somNews.store']) !!}
+
+            <div class="card-header">
+                <span><i class="far fa-newspaper ml-2"></i> Add News</span>
+            </div>
 
             <div class="card-body">
 
@@ -28,8 +40,9 @@
             </div>
 
             <div class="card-footer">
+                <a href="{{ route('somNews.index') }}" class="btn btn-default"><i class="fa fa-chevron-left"></i> Back</a>
+                <a href="#" class="btn btn-secondary">Save & Add More</a>
                 {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('somNews.index') }}" class="btn btn-default">Cancel</a>
             </div>
 
             {!! Form::close() !!}
