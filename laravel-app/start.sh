7456589836108086eaf694a0110e8cbb106800b8
Andrés Bestard <a.bestard@innovation.es>
2021-07-06 14:01:12 +0200

    updated script

--- laravel-app/start.sh
+++ laravel-app/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+php artisan optimize &&
 php artisan route:clear &&  
 php artisan view:clear && 
 php artisan config:clear &&
 php artisan cache:clear && 
-php artisan clear-compiled
+php artisan clear-compiled &&
+php artisan serve --host 0.0.0.0
