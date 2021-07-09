4888d4b688a6534a15dbb09a76e329870530b9b5
rightvalue <bluebird6922@gmail.com>
2021-07-09 03:33:58 +0300

    update projects_advisors

--- laravel-app/resources/views/som_projects_advisors/create.blade.php
+++ laravel-app/resources/views/som_projects_advisors/create.blade.php
@@ -5,13 +5,21 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>Create Som Projects Advisors</h1>
+                    <h1>Create Projects Advisors</h1>
                 </div>
             </div>
         </div>
     </section>
 
     <div class="content px-3">
+
+        <div class="row">
+            <div class="col-12-lg ml-2 mb-4">
+                <a href="{{ route('somProjectsAdvisors.index') }}">
+                    <i class="fa fa-chevron-left"></i> Back To List Data Projects Advisors
+                </a>
+            </div>
+        </div>
 
         @include('adminlte-templates::common.errors')
 
@@ -21,15 +29,17 @@
 
             <div class="card-body">
 
-                <div class="row">
-                    @include('som_projects_advisors.fields')
-                </div>
+                @include('som_projects_advisors.fields')
 
             </div>
 
             <div class="card-footer">
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('somProjectsAdvisors.index') }}" class="btn btn-default">Cancel</a>
+                <div class="row">
+                    <div class="offset-sm-2 col-sm-10">
+                        {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+                        <a href="{{ route('somProjectsAdvisors.index') }}" class="btn btn-default">Cancel</a>
+                    </div>
+                </div>    
             </div>
 
             {!! Form::close() !!}
--- laravel-app/resources/views/som_projects_advisors/fields.blade.php
+++ laravel-app/resources/views/som_projects_advisors/fields.blade.php
@@ -1,11 +1,76 @@
 <!-- Name Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('name', 'Name') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
 </div>
 
-<!-- Som Projects Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('som_projects_id', 'Som Projects Id:') !!}
-    {!! Form::number('som_projects_id', null, ['class' => 'form-control']) !!}
+<!-- Email Field -->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('email', 'Email') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('email', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
+</div>
+
+<!-- Phone Number Field -->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('phone_number', 'Phone number') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('phone_number', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
+</div>
+
+<!-- Other information Field -->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('other_info', 'Other information') !!}
+        <span class="required">&nbsp;</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('other_info', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
+</div>
+
+<!-- Company-->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('company', 'Company') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('company', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
+</div>
+
+<!-- Company Profile-->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('company_profile', 'Company Profile') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('company_profile', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
+</div>
+
+<!-- Role -->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('role', 'Role in the Project') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('role', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
 </div>
