6e8eb44618331718f6fbee0dd14eb81a17723a82
rightvalue <bluebird6922@gmail.com>
2021-07-09 03:58:37 +0300

    update projects_phases

--- laravel-app/resources/views/som_projects_phases/edit.blade.php
+++ laravel-app/resources/views/som_projects_phases/edit.blade.php
@@ -13,6 +13,14 @@
 
     <div class="content px-3">
 
+        <div class="row">
+            <div class="col-12-lg ml-2 mb-4">
+                <a href="{{ route('somProjectsPhases.index') }}">
+                    <i class="fa fa-chevron-left"></i> Back To List Data Projects Phases
+                </a>
+            </div>
+        </div>
+
         @include('adminlte-templates::common.errors')
 
         <div class="card">
@@ -23,6 +31,15 @@
                 <div class="row">
                     @include('som_projects_phases.fields')
                 </div>
+            </div>
+
+            <div class="card-footer">
+                <div class="row">
+                    <div class="offset-sm-2 col-sm-10">
+                        {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+                        <a href="{{ route('somProjectsPhases.index') }}" class="btn btn-default">Cancel</a>
+                    </div>
+                </div>    
             </div>
 
             <div class="card-footer">
