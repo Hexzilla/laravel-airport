3eb4aadf1a4c4e09ab6b0f5f862e5d976e67dfdd
rightvalue <bluebird6922@gmail.com>
2021-07-09 03:52:59 +0300

    update projects_milestones

--- laravel-app/resources/views/som_projects_milestones/fields.blade.php
+++ laravel-app/resources/views/som_projects_milestones/fields.blade.php
@@ -1,25 +1,31 @@
 <!-- Som Projects Phases Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('som_projects_phases_id', 'Som Projects Phases Id:') !!}
-    {!! Form::number('som_projects_phases_id', null, ['class' => 'form-control']) !!}
-</div>
+{{ Form::hidden('som_projects_phases_id', 'som_projects_phases_id') }}
 
-<!-- Blocking Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('blocking', 'Blocking:') !!}
-    {!! Form::number('blocking', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Order Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('order', 'Order:') !!}
-    {!! Form::number('order', null, ['class' => 'form-control']) !!}
+<!-- Name Field -->
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('name', 'Name') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+    </div>
 </div>
 
 <!-- Due Date Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('due_date', 'Due Date:') !!}
-    {!! Form::text('due_date', null, ['class' => 'form-control','id'=>'due_date']) !!}
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('due_date', 'Due Date') !!}
+        <span class="required">*</span>
+    </div>
+    <div class="col-sm-10">
+        <div class="input-group mb-2">
+            <div class="input-group-prepend">
+                <div class="input-group-text">@</div>
+            </div>
+            {!! Form::text('due_date', null, ['class' => 'form-control','id'=>'due_date']) !!}
+        </div>
+    </div>
 </div>
 
 @push('page_scripts')
@@ -32,23 +38,16 @@
     </script>
 @endpush
 
-<!-- Name Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
-</div>
-
-<!-- Som Status Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('som_status_id', 'Som Status Id:') !!}
-    {!! Form::number('som_status_id', null, ['class' => 'form-control']) !!}
-</div>
 
 <!-- Is Hidden Field -->
-<div class="form-group col-sm-6">
-    <div class="form-check">
-        {!! Form::hidden('is_hidden', 0, ['class' => 'form-check-input']) !!}
-        {!! Form::checkbox('is_hidden', '1', null, ['class' => 'form-check-input']) !!}
-        {!! Form::label('is_hidden', 'Is Hidden', ['class' => 'form-check-label']) !!}
+<div class="form-group row">
+    <div class="col-sm-2 col-form-label text-right">
+        {!! Form::label('is_hidden', 'Is Hidden') !!}
+        <span class="required">&nbsp</span>
+    </div>
+    <div class="col-sm-10">
+        <div class="form-check">
+            {!! Form::checkbox('is_hidden', null, true, array('id' => 'is_hidden', 'class' => 'form-check-input')) !!}
+        </div>
     </div>
 </div>
