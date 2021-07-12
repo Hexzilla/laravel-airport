43af8c578f242c334aec2b42fa09fd7bb9d17558
Joseph A. Poehlein López <ja.poehlein@innovation.es>
2021-07-12 10:54:36 +0200

    Added encryption endpoint.

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('encrypt/{text}', [App\Http\Controllers\EncryptController::class, 'encrypt'], function ($text) {
+    return $text;
+});
