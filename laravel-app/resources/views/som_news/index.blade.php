dc5c900c0dbc86c77694df2fcb44b0dd16ef7332
Joseph A. Poehlein López <ja.poehlein@innovation.es>
2021-07-20 16:32:13 +0200

    Styling.

--- laravel-app/resources/views/som_news/index.blade.php
+++ laravel-app/resources/views/som_news/index.blade.php
@@ -1,66 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1><i class="far fa-newspaper ml-2"></i> News</h1>
-                </div>
-                <div class="col-sm-6">
-                    <div class="float-right">                                                
-                        <a href="#">
-                            <i class="fa fa-palette"></i> Home
-                        </a>
-                        <a href="#">
-                            <i class="fa fa-chevron-right"></i> News
-                        </a>                        
-                    </div>                        
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <div class="content px-3">
-        <div class="row ml-1">
-            <div class="col-md-6 mb-4">
-                <a href="#">
-                    <i class="fa fa-chevron-left"></i> Home
-                </a>
-            </div>
-            <div class="col-md-6 mb-4">
-                <a class="btn btn-primary float-right"
-                    href="{{ route('somNews.create') }}">
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>
+                    <i class="far fa-newspaper ml-2"></i> News
+                    <a class="btn btn-primary"
+                        href="{{ route('somNews.create') }}">
                         Add New
-                </a>
+                    </a>
+                </h1>
             </div>
-        </div>
-
-        @include('flash::message')
-
-        <div class="clearfix"></div>
-
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col-md-3">
-                        <span><i class="far fa-newspaper ml-2"></i> News List</span>
-                    </div>
-                    <div class="col-md-9"></div>
-                </div>                
-            </div>
-            <div class="card-body p-0">
-                @include('som_news.table')
-
-                <div class="card-footer clearfix float-right">
-                    <div class="float-right">
-
-                    </div>
+            <div class="col-sm-6">
+                <div class="float-right">
+                    <a href="#">
+                        <i class="fa fa-palette"></i> Home
+                    </a>
+                    <a href="#" style="color:black; cursor: text; text-decoration: none;">
+                        <i class="fa fa-chevron-right"></i> News
+                    </a>
                 </div>
             </div>
+        </div>
+    </div>
+</section>
 
+<div class="content px-3">
+    <div class="row ml-1">
+        <div class="col-md-6 mb-4">
+            <a href="#">
+                <i class="fa fa-chevron-left"></i> Home
+            </a>
         </div>
     </div>
 
-@endsection
+    @include('flash::message')
 
+    <div class="clearfix"></div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-3">
+                    <span><i class="far fa-newspaper ml-2"></i> News List</span>
+                </div>
+                <div class="col-md-9"></div>
+            </div>
+        </div>
+        <div class="card-body p-0">
+            @include('som_news.table')
+
+            <div class="card-footer clearfix float-right">
+                <div class="float-right">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
