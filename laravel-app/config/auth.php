29985a7ba3bd6b717f4e80aae1878c9ae9b785a2
ShkuratovU <ShkuratovMong@mail.ru>
2021-07-23 11:22:07 +0900

    auth.php

--- laravel-app/config/auth.php
+++ laravel-app/config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent', //'ldap', // was 'eloquent'
+            'driver' => 'ldap', // was 'eloquent'
             'model' => App\Models\CmsUsers::class,
         ],
 
